159cfefd3151a84e163fcb6b412940b7d063683e
“Carla <carlatorrico@gmail.com>
2017-09-25 10:20:03 -0300

    - inserindo google analytics

--- views/includes/header.php
+++ views/includes/header.php
@@ -20,6 +20,16 @@ if ( ! defined('ABSPATH')) exit; ?>
 <!--<![endif]-->
 
 <head>
+    <!-- Global Site Tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106970041-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)};
+        gtag('js', new Date());
+
+        gtag('config', 'UA-106970041-1');
+    </script>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width">
@@ -31,15 +41,6 @@ if ( ! defined('ABSPATH')) exit; ?>
     <title><?php echo $this->title; ?></title>
 </head>
 <body>
-<!---->
-<!--<!-- Global Site Tag (gtag.js) - Google Analytics -->
-<!--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106970041-1"></script>-->
-<!--<script>-->
-<!--  window.dataLayer = window.dataLayer || [];-->
-<!--  function gtag(){dataLayer.push(arguments)};-->
-<!--  gtag('js', new Date());-->
-<!--  gtag('config', 'UA-106970041-1');-->
-<!--</script>-->
 
 <header>
     <section class="view hm-stylish-strong">
