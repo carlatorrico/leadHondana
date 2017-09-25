3ad5b0b036985ba696c6781aff9a112456fd011f
“Carla <carlatorrico@gmail.com>
2017-09-25 12:57:23 -0300

    - adicionando widget redes sociais

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -7,26 +7,41 @@
  */
 
 if ( ! defined('ABSPATH')) exit; ?>
+    <hr class="c-hr">
 
+<div class="container wow fadeInUp" data-wow-delay="0.3s">
+    <div class="row justify-content-md-center">
+        <div class="col-12 text-center my-4">
+            <h2 class="title">Encontre-nos nas redes sociais</h2>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-2 justify-content-md-center">
+            <a href="https://www.facebook.com/oficial.rhdofuturo/" target="_blank" title="Facebook">
+                <h4 class="section-title">Facebook</h4>
+            </a>
+            <div class="fb-page" data-href="https://www.facebook.com/oficial.rhdofuturo/" data-tabs="timeline" data-width="360" data-height="205" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/oficial.rhdofuturo/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/oficial.rhdofuturo/">RH do Futuro</a></blockquote></div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-2 justify-content-md-center">
+            <a href="https://www.linkedin.com/company-beta/25168595/" target="_blank" title="LinkedIn">
+                <h4 class="section-title">LinkedIn</h4>
+            </a>
+            <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+            <script type="IN/CompanyProfile" data-id="25168595" data-format="inline" data-related="false"></script>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-2 justify-content-md-center">
+            <a href="https://www.instagram.com/rhdofuturo/" target="_blank" title="Instagram">
+                <h4 class="section-title">Instagram</h4>
+            </a>
+            <script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/789a27309cba5746be857c15ea6dbcf7.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+        </div>
+    </div>
+</div>
 </section>
 
 <div class="line"></div>
 <hr class="c-hr">
-
 <footer id="footer">
-<!--    <div class="container text-center>"-->
-<!--        <div class="row">-->
-<!--            <div class="col-md-4 col-sm-4 col-xs-12">-->
-<!--                <div class="top-left">-->
-<!--                    <a href="https://www.facebook.com/" target="_blank"><img src="--><?php //echo HOME_URI;?><!--/assets/img/fb64azul.png"></a>-->
-<!--                    <a href="https://www.instagram.com/" target="_blank"><img src="--><?php //echo HOME_URI;?><!--/assets/img/ins64azul.png"></a>-->
-<!--                    <a href="https://www.linkedin.com/" target="_blank"><img src="--><?php //echo HOME_URI;?><!--/assets/img/in64azul.png"></a>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
     <div class="container">
-        <div class="row py-5 text-center">
+        <div class="row py-3 text-center">
             <div class="col-md-12 col-xs-12">
                 <p class="info">2017 &copy; RH do Futuro.</p>
             </div>
