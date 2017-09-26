711610b65fb2a78a7b124a15414ae8cb54bad0f5
“Carla <carlatorrico@gmail.com>
2017-09-26 18:43:29 -0300

    - alterando funcao getIP

--- models/lead/lead-model.php
+++ models/lead/lead-model.php
@@ -110,8 +110,8 @@ class LeadModel
     }
 
     public function getIP() {
-        if (getenv("HTTP_CLIENT_IP")) $ip = getenv("HTTP_CLIENT_IP");
-        else if(getenv("HTTP_X_FORWARDED_FOR")) $ip = getenv("HTTP_X_FORWARDED_FOR");
+        if(getenv("HTTP_X_FORWARDED_FOR")) $ip = getenv("HTTP_X_FORWARDED_FOR");
+        else if (getenv("HTTP_CLIENT_IP")) $ip = getenv("HTTP_CLIENT_IP");
         else if(getenv("REMOTE_ADDR")) $ip = getenv("REMOTE_ADDR");
         else $ip = "UNKNOWN";
         return $ip;
