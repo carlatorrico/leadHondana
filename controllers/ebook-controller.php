8bb0b530c38ef85a2fe1f7ab2ba50bcb2dae77ed
“Carla <carlatorrico@gmail.com>
2017-09-26 00:14:30 -0300

    - download de ebook

--- controllers/ebook-controller.php
+++ controllers/ebook-controller.php
@@ -5,3 +5,51 @@
  * Date: 25/09/17
  * Time: 23:13
  */
+
+class EbookController extends MainController
+{
+    public function index()
+    {
+        // Page title
+        $this->title = 'Blog';
+
+        // Carrega o modelo para este view
+        $modelo = $this->load_model('blog/blog-model');
+
+        // /views/includes/header.php
+        require ABSPATH . '/views/includes/header.php';
+
+        // /views/books-register/index.php
+        require ABSPATH . '/views/ebook/ebook-view.php';
+
+        // /views/includes/footer.php
+        require ABSPATH . '/views/includes/footer.php';
+    }
+
+    public function download(){
+
+        // Page title
+        $this->title = 'RH do Futuro';
+
+        // Carrega o modelo para este view
+        $modelo = $this->load_model('lead/lead-model');
+        $modelo->insert();
+
+        header("Content-Type: application/octet-stream");
+
+        $file = "bmc.pdf";
+        header("Content-Disposition: attachment; filename=" . urlencode($file));
+        header("Content-Type: application/octet-stream");
+        header("Content-Type: application/download");
+        header("Content-Description: File Transfer");
+        header("Content-Length: " . filesize($file));
+        flush();
+        $fp = fopen($file, "r");
+        while (!feof($fp))
+        {
+            echo fread($fp, 65536);
+            flush(); // this is essential for large downloads
+        }
+        fclose($fp);
+    }
+}
--- views/ebook/ebook-view.php
+++ views/ebook/ebook-view.php
@@ -9,7 +9,6 @@
 if (!defined('ABSPATH')) {
     exit;
 }
-$lista = $modelo->get_blog_detail($uri);
 $blogs = $modelo->get_blog_list();
 ?>
 
@@ -17,47 +16,35 @@ $blogs = $modelo->get_blog_list();
     <section class="section extra-margins pb-3 text-center text-lg-left">
 
         <div class="row mt-5">
-            <?php if (!empty($lista)): ?>
-                <?php foreach ($lista as $fetch_blog): ?>
-                    <div class="col-lg-7 mb-4">
-                        <div class="view overlay hm-white-slight z-depth-1-half mb-3">
-                            <img src="<?php echo $fetch_blog['image']; ?>" class="img-fluid">
-                        </div>
-                        <!--                        <h4 class="mb-3"><strong>-->
-                        <?php //echo $fetch_blog['title'] ?><!--</strong></h4>-->
-                        <?php echo $fetch_blog['content'] ?>
-
-                        <div class="card card-block elegant-color mt-3">
-                            <form class="form-inline pl-auto d-flex justify-content-center mt-4" action="<?php echo HOME_URI; ?>/lead" method="POST">
-                                <div class="md-form form-group">
-                                    <input type="text" id="name" name="name" class="form-control text-white" required>
-                                    <label class="text-white">Nome</label>
-                                </div>
-                                <div class="md-form form-group">
-                                    <input type="text" id="sobrenome" name="sobrenome" class="form-control text-white" required>
-                                    <label class="text-white">Sobrenome</label>
-                                </div>
-                                <div class="md-form form-group">
-                                    <input type="email" id="email" name="email" class="form-control text-white" required>
-                                    <label class="text-white">E-mail</label>
-                                </div>
-                                <div class="md-form form-group">
-                                    <div class="text-center">
-                                        <button class="btn btn-info mb-3">Cadastrar</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-
-            <?php else: ?>
-                <div class="col-lg-7">
-                    <div class="mb-4 alert alert-warning mt-4">
-                        <strong>Desculpe-nos, o conteúdo selecionado não está mais disponível.</strong>
-                    </div>
+            <div class="col-lg-7 mb-4">
+                <div class="view overlay hm-white-slight z-depth-1-half mb-3">
+                    <img src="<?php echo HOME_URI; ?>/assets/img/4_blog_ebook.png" class="img-fluid">
                 </div>
-            <?php endif; ?>
+                <h1 class="mb-3 cyan-text"><strong>Quero baixar o e-book</strong></h1>
+
+                <form action="<?php echo HOME_URI;?>/ebook/download" method="POST">
+                    <div class="md-form">
+                        <i class="fa fa-user prefix grey-text"></i>
+                        <input type="text" id="name" name="name" class="form-control" lettersonly required>
+                        <label>Nome</label>
+                    </div>
+                    <div class="md-form">
+                        <i class="fa fa-user prefix grey-text"></i>
+                        <input type="text" id="sobrenome" name="sobrenome" class="form-control" required>
+                        <label>Sobrenome</label>
+                    </div>
+                    <div class="md-form">
+                        <i class="fa fa-envelope prefix grey-text"></i>
+                        <input type="email" id="email" name="email" class="form-control" required>
+                        <label>E-mail</label>
+                    </div>
+
+                    <div class="text-center">
+                        <button class="btn btn-info mb-3">Cadastrar</button>
+                    </div>
+                </form>
+            </div>
+
 
             <div class="col-lg-4 ml-xl-4 mb-4">
                 <?php foreach ($blogs as $linha): ?>
