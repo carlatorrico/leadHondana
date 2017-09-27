68608556559cf7de7070c93555cbd06ae29d944b
“Carla <carlatorrico@gmail.com>
2017-09-27 15:39:26 -0300

    - atualizando servidor de testes

--- views/includes/header.php
+++ views/includes/header.php
@@ -20,15 +20,15 @@ if ( ! defined('ABSPATH')) exit; ?>
 <!--<![endif]-->
 
 <head>
-<!--    <!-- Google Tag Manager -->-->
+<!--    <!-- Google Tag Manager -->
 <!--    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':-->
 <!--            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],-->
 <!--            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=-->
 <!--            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);-->
 <!--        })(window,document,'script','dataLayer','GTM-P74X72P');</script>-->
-<!--    <!-- End Google Tag Manager -->-->
+<!--    <!-- End Google Tag Manager -->
 <!---->
-<!--    <!-- Global Site Tag (gtag.js) - Google Analytics -->-->
+<!--    <!-- Global Site Tag (gtag.js) - Google Analytics -->
 <!--    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106970041-1"></script>-->
 <!--    <script>-->
 <!--        window.dataLayer = window.dataLayer || [];-->
@@ -77,11 +77,11 @@ if ( ! defined('ABSPATH')) exit; ?>
 
 <body>
 
-<!--<!-- Google Tag Manager (noscript) -->-->
+<!--<!-- Google Tag Manager (noscript) -->
 <!--<noscript>-->
 <!--    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P74X72P" height="0" width="0" style="display:none;visibility:hidden"></iframe>-->
 <!--</noscript>-->
-<!--<!-- End Google Tag Manager (noscript) -->-->
+<!--<!-- End Google Tag Manager (noscript) -->
 
 <div id="fb-root"></div>
 
